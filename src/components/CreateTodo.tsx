b979c0bd3a9c7b8dac386e503005e014cb4f6c42
Ross Jenkins <rjenkins96@live.co.uk>
2024-06-20 23:31:34 +0100

    adds example of useFormStatus

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,4 +1,15 @@
 import { FC } from 'react';
+import { useFormStatus } from 'react-dom';
+
+const SubmitButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+        <button type="submit" disabled={pending}>
+            Create todo
+        </button>
+    );
+};
 
 interface CreateTodoProps {
     createTodoAction: (formData: FormData) => void;
@@ -6,7 +17,7 @@ interface CreateTodoProps {
     isError: boolean;
 }
 
-export const CreateTodo: FC<CreateTodoProps> = ({createTodoAction, isPending, isError}) => (
+export const CreateTodo: FC<CreateTodoProps> = ({ createTodoAction, isPending, isError }) => (
     <form className="todo-form" action={createTodoAction}>
         <input
             className="todo-input"
@@ -22,9 +33,7 @@ export const CreateTodo: FC<CreateTodoProps> = ({createTodoAction, isPending, is
             placeholder="Description"
             required={true}
         />
-        <button type="submit" disabled={isPending}>
-            Create todo
-        </button>
+        <SubmitButton/>
         {isPending && <div className="creating">Adding...</div>}
         {isError && <div className="error">Something went wrong...</div>}
     </form>
